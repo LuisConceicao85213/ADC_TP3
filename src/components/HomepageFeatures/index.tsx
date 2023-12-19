aba201147dc7aefffbcf83a8439d04692d17a7e1
DavidMarques <a85229@ualg.pt>
2023-12-19 20:48:21 +0000

    frase da home page

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -23,8 +23,7 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Na nossa oficina, a velocidade não é apenas uma promessa, é a nossa expertise. Serviços eficazes e rápidos que garantem a excelência, proporcionando uma experiência automotiva única e eficiente..
       </>
     ),
   },
