cfbed8be9a61ea8d9be577d63931b0f7d326c503
LuisConceicao85213 <a85213@ualg.pt>
2023-12-20 22:03:02 +0000

    sidebar não funcionou

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,7 +75,6 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            hideable: true,
             label: 'Serviços',
           },
           {to: '/blog', label: 'Quem Somos', position: 'left'},
