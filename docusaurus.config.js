5e32f7e610dd6cab83383dd330f4aec786ee026c
LuisConceicao85213 <a85213@ualg.pt>
2023-12-21 01:39:47 +0000

    intro docs footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -127,7 +127,7 @@ const config = {
             items: [
               {
                 label: 'Serviços',
-                to: '/docs',
+                to: '/docs/intro',
               },
               {
                 label: 'Quem Somos',
